fd5d48566a417c56d07402eeca6f35bcf34dc8e9
Ailing Zhang <ailzhang@fb.com>
2019-06-01 15:58:25 -0700

    enable pytorch test on code snippets

---
+++ scripts/run_pytorch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+. ~/miniconda3/etc/profile.d/conda.sh
+conda activate base
+
+ALL_FILE=$(find *.md ! -name README.md)
+TEMP_PY="temp.py"
+GANs="facebookresearch_pytorch-gan-zoo"
+
+for f in $ALL_FILE
+do
+  echo "Running pytorch example in $f"
+  # FIXME: GAN models checkpoints are on cuda.
+  if [[ $f = $GANs* ]]; then
+    echo "...skipped due to cuda checkpoints."
+  else
+    sed -n '/^```/,/^```/ p' < $f | sed '/^```/ d' > $TEMP_PY
+    python $TEMP_PY
+
+    if [ -f "$TEMP_PY" ]; then
+      rm $TEMP_PY
+    fi
+  fi
+done
