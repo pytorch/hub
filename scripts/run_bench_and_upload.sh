916372e186361e275bee52e030d2b6c8ff8cd822
Will Constable <whc@fb.com>
2020-08-20 12:05:02 -0700

    Contition scribe upload on presence of token

--- scripts/run_bench_and_upload.sh
+++ scripts/run_bench_and_upload.sh
@@ -6,4 +6,11 @@ conda activate base
 BENCHMARK_DATA=".benchmarks"
 mkdir -p ${BENCHMARK_DATA}
 pytest benchmark/test_bench.py --benchmark-sort=Name --benchmark-json=${BENCHMARK_DATA}/hub.json
-python benchmark/upload_scribe.py --pytest_bench_json ${BENCHMARK_DATA}/hub.json
+
+# Token is only present for certain jobs, only upload if present
+if [ -z "$SCRIBE_GRAPHQL_ACCESS_TOKEN" ]
+then
+    echo "Skipping benchmark upload, token is missing."
+else
+    python benchmark/upload_scribe.py --pytest_bench_json ${BENCHMARK_DATA}/hub.json
+fi
