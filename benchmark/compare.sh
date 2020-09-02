2b1341159723461a237c5f25ac4e99afb25253b7
Will Constable <whc@fb.com>
2020-09-01 21:09:13 -0700

    Remove benchmark models, now in pytorch/benchmark repo
    See https://github.com/pytorch/benchmark.

--- benchmark/compare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pytest test_bench.py -k cuda-jit --fuser legacy --benchmark-json legacy.json
-pytest test_bench.py -k cuda-jit --fuser te --benchmark-json te.json
-python compare.py legacy.json te.json
--- scripts/install_nightlies.sh
+++ scripts/install_nightlies.sh
@@ -4,5 +4,4 @@ set -e
 . ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
 conda install -y pytorch torchtext torchvision -c pytorch-nightly
-pip install -q pytest pytest-benchmark requests
 
