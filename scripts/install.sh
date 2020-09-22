9146b39dec42f6ff7080ef0f756b956b5dd67dcd
Alexander Veysov <aveysov@gmail.com>
2020-09-22 18:03:31 +0300

    Add Silero Speech-To-Text models (#153)
    * Add Silero Speech-To-Text models
    
    * Move the MD file to root folder
    
    * Fix util imports, fix models.yml path issues
    
    * Add TorchAudio to the test environment
    
    * Add -y to TorchAudio install
    
    * Add OmegaConf installation to build
    
    * Add dependencies installation, add one file example

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,8 @@ conda activate base
 
 # Install the latest release of pytorch and torchvision
 conda install -y pytorch torchvision -c pytorch
+# Also install torchaudio
+conda install -y -c pytorch torchaudio
 
 # Dependencies required to load models
 conda install -y regex pillow tqdm boto3 requests numpy\
@@ -14,3 +16,4 @@ conda install -y -c conda-forge librosa inflect
 
 pip install -q fastBPE sacremoses sentencepiece subword_nmt editdistance
 pip install -q visdom mistune filelock tokenizers packaging
+pip install -q omegaconf
