65702208805c76230cdb01284810dc650e72c9b0
szq0214 <zhiqiangshen0214@gmail.com>
2020-10-30 22:49:34 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,4 +18,5 @@ pip install -q fastBPE sacremoses sentencepiece subword_nmt editdistance
 pip install -q visdom mistune filelock tokenizers packaging
 pip install -q omegaconf
 pip install -q hydra-core
+pip install -q --upgrade google-api-python-client
 pip install -q timm
