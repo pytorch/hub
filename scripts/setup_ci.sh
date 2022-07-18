3435719cce92780df9747b934c8d8bb50d32c52c
Nicolas Hug <contact@nicolas-hug.com>
2022-07-18 10:39:11 +0100

    Try new things

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -2,11 +2,17 @@
 set -ex -o pipefail
 
 # Set up NVIDIA docker repo. See https://nvidia.github.io/libnvidia-container/
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-         && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-         && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-               sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-               sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+#          && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+#          && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+#                sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+#                sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+# ubuntu=ubuntu20.04
 # curl -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 # echo "deb https://nvidia.github.io/libnvidia-container/$ubuntu/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
 # echo "deb https://nvidia.github.io/nvidia-container-runtime/$ubuntu/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
