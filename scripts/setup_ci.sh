73eb300805482532211d8dfa66e035e237a0842f
Nicolas Hug <contact@nicolas-hug.com>
2023-01-05 17:47:09 +0000

    UGGGH

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -32,20 +32,22 @@ sudo rm -f /etc/apt/sources.list.d/nvidia-docker.list*
 
 curl https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 
-sudo apt-get -y update
-# sudo apt-get -y remove linux-image-generic linux-headers-generic linux-generic docker-ce
-sudo apt-get -y remove --force-yes linux-image-generic linux-headers-generic linux-generic docker-ce
-sudo apt-get -y install \
-  linux-headers-$(uname -r) \
-  linux-image-generic \
-  moreutils \
-  nvidia-container-runtime \
-  nvidia-docker2 \
-  expect-dev
+FAIL_HERE
 
-sudo pkill -SIGHUP dockerd
+# sudo apt-get -y update
+# # sudo apt-get -y remove linux-image-generic linux-headers-generic linux-generic docker-ce
+# sudo apt-get -y remove --force-yes linux-image-generic linux-headers-generic linux-generic docker-ce
+# sudo apt-get -y install \
+#   linux-headers-$(uname -r) \
+#   linux-image-generic \
+#   moreutils \
+#   nvidia-container-runtime \
+#   nvidia-docker2 \
+#   expect-dev
 
-DRIVER_FN="NVIDIA-Linux-x86_64-450.51.06.run"
-wget "https://s3.amazonaws.com/ossci-linux/nvidia_driver/$DRIVER_FN"
-sudo /bin/bash "$DRIVER_FN" -s --no-drm || (sudo cat /var/log/nvidia-installer.log && false)
-nvidia-smi
+# sudo pkill -SIGHUP dockerd
+
+# DRIVER_FN="NVIDIA-Linux-x86_64-450.51.06.run"
+# wget "https://s3.amazonaws.com/ossci-linux/nvidia_driver/$DRIVER_FN"
+# sudo /bin/bash "$DRIVER_FN" -s --no-drm || (sudo cat /var/log/nvidia-installer.log && false)
+# nvidia-smi
