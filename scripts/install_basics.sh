5eaab4ba308411b6a2cfb339e3917f08a42f0778
Nicolas Hug <contact@nicolas-hug.com>
2023-01-06 18:50:23 +0000

    Force bash?

--- scripts/install_basics.sh
+++ scripts/install_basics.sh
@@ -10,7 +10,7 @@ CONDA=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 filename=$(basename "$CONDA")
 wget "$CONDA"
 chmod +x "$filename"
-./"$filename" -b -u
+bash "$filename" -b -u
 
 # Force to use python3.8
 . ~/miniconda3/etc/profile.d/conda.sh
