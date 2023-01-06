92fe65eec4b8075c7a81d5a9bf978eacb6886e9b
Nicolas Hug <contact@nicolas-hug.com>
2023-01-06 19:05:17 +0000

    Looks like it was fine-ish

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# set -ex -o pipefail
-set -x
+set -ex -o pipefail
 
 # Set up NVIDIA docker repo. See https://nvidia.github.io/libnvidia-container/
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
@@ -8,19 +7,6 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
          && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
                sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
                sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-
-# curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-# curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-#   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-# ubuntu=ubuntu20.04
-# curl -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-# echo "deb https://nvidia.github.io/libnvidia-container/$ubuntu/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
-# echo "deb https://nvidia.github.io/nvidia-container-runtime/$ubuntu/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
-# echo "deb https://nvidia.github.io/nvidia-docker/$ubuntu/amd64 /" | sudo tee -a /etc/apt/sources.list.d/nvidia-docker.list
-
-
 
 
 # Remove unnecessary sources
@@ -34,20 +20,17 @@ sudo rm -f /etc/apt/sources.list.d/nvidia-docker.list*
 curl https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 
 sudo apt-get -y update
-# sudo apt-get -y remove linux-image-generic linux-headers-generic linux-generic docker-ce
-sudo apt-get -y remove --force-yes linux-image-generic linux-headers-generic linux-generic docker-ce
+sudo apt-get -y remove linux-image-generic linux-headers-generic linux-generic docker-ce
 sudo apt autoremove
-sudo apt-get -y install linux-headers-5.4.0-135-generic
 sudo apt-get -y install \
-  linux-headers-$(uname -r) \
+  linux-headers-5.4.0-135-generic \
   linux-image-generic \
   moreutils \
   nvidia-container-runtime \
   nvidia-docker2 \
   expect-dev
 
-# sudo systemctl status docker
-sudo pkill -SIGHUP dockerd
+# sudo pkill -SIGHUP dockerd
 
 DRIVER_FN="NVIDIA-Linux-x86_64-450.51.06.run"
 wget "https://s3.amazonaws.com/ossci-linux/nvidia_driver/$DRIVER_FN"
